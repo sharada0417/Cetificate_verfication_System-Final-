16511fbcb8e0e53742e042d3ad91d9d96a90a153
sharada417 <sharada.kanchana2001@gmail.com>
2025-07-10 15:25:02 +0530

    create Event model

---
+++ model/Event.js
@@ -0,0 +1,27 @@
+const mongoose = required('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    eventId :{
+        type :String,
+        required:true,
+        unique:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    organizer:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Event',eventSchema)
